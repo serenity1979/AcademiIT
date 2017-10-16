81c8910b53fd01739e2067d978afd808acdba8db
Serenity1979 <serenity1979@mail.ru>
2017-10-17 01:01:54 +0700

    control2.2

---
+++ Spiral.py
@@ -0,0 +1,39 @@
+
+"""
+Выведите таблицу размером n×n, заполненную числами от 1 до n 2 по спирали, выходящей из левого верхнего угла и закрученной по часовой стрелке, как показано в примере (здесь n=5):
+Sample Input:
+5
+Sample Output:
+1 2 3 4 5
+16 17 18 19 6
+15 24 25 20 7
+14 23 22 21 8
+13 12 11 10 9
+
+"""
+n=int(input())
+a=[[0 for m in range(n)] for i in range(n)]
+i=1
+j=int(n/2)
+k=int(n/2)
+p=int(n/2)
+for k in range(1,p+1):
+    for j in range(k-1,n-k+1):
+        a[k-1][j] = i
+        i+=1
+    for j in range(k,n-k+1):
+        a[j][n-k] = i
+        i+=1
+    for j in range(n-k-1,k-2,-1):
+        a[n-k][j] = i
+        i+=1
+    for j in range(n-k-1,k-1,-1):
+        a[j][k-1] = i
+        i+=1
+if n%2==1:
+    a[p][p]=n*n
+for i in range(n):
+    for l in range(n):
+        print(a[i][l], end=' ')
+    print()
+
