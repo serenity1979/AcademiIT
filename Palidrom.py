d52b5671f5025e412e23588ecd944f429770382a
Serenity1979 <serenity1979@mail.ru>
2018-05-14 23:02:33 +0700

    Следующие задачи из 3 раздела, продолжение

---
+++ Palidrom.py
@@ -0,0 +1,32 @@
+'''
+Палидром: Слово или фраза, которые одинаково читаются слева направо и справа налево.
+GATTAG
+
+YES
+'''
+
+s = input()
+i = 0
+j = len(s) - 1
+is_palindrom = True
+while i < j:
+    if s[i] != s[j]:
+        is_palindrom = False
+    i += 1
+    j -= 1
+if is_palindrom:
+    print('YES')
+else:
+    print('NO')
+
+
+'''
+Альтернатива:
+s = input()
+r = s[::-1]
+if s == r:
+    print('YES')
+else:
+    print('NO')
+
+'''
